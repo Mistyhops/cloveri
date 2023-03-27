fd8ff0e0127130d839e79a018a5df9e7249166ba
Mistyhops <nedgalkin@gmail.com>
2023-03-28 00:34:51 +0300

    updated readme file

--- test_db.sql
+++ test_db.sql
@@ -1,16 +1,3 @@
--- DROP TABLE IF EXISTS auth_group CASCADE;
--- DROP TABLE IF EXISTS auth_group_permissions CASCADE;
--- DROP TABLE IF EXISTS auth_permission CASCADE;
--- DROP TABLE IF EXISTS auth_user CASCADE;
--- DROP TABLE IF EXISTS auth_user_groups CASCADE;
--- DROP TABLE IF EXISTS auth_user_user_permissions CASCADE;
--- DROP TABLE IF EXISTS django_admin_log CASCADE;
--- DROP TABLE IF EXISTS django_content_type CASCADE;
--- DROP TABLE IF EXISTS django_migrations CASCADE;
--- DROP TABLE IF EXISTS django_session CASCADE;
--- DROP TABLE IF EXISTS tree_structure_node CASCADE;
-
-
 CREATE TABLE tree_structure_node(
 	id				bigserial	NOT NULL,
 	path			text		NOT NULL,
@@ -30,7 +17,3 @@ CREATE UNIQUE INDEX node_id_project_id_item_type_item
 
 CREATE INDEX node_project_id_item_type_item
 	ON tree_structure_node(project_id, item_type, item);
-
-
--- SELECT * FROM tree_structure_node
--- 	ORDER BY path;
