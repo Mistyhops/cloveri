3112821532a3ad374859347358d1d398a0c79f69
Di Sk <disk@MacBook-Air-Di.Dlink>
2023-04-01 16:32:49 +0200

    fff

--- start_project/settings.py
+++ start_project/settings.py
@@ -79,16 +79,7 @@ WSGI_APPLICATION = 'start_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cloveri_db_3',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+
 
 
 # Password validation
@@ -131,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
