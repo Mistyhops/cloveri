1564ea2004fbdb916b4c5e9702b4c1b11c9ebef6
Di Sk <disk@MacBook-Air-Di.Dlink>
2023-04-07 23:27:22 +0200

    added responce param level node

--- tree_structure/serializers.py
+++ tree_structure/serializers.py
@@ -4,15 +4,18 @@ from .models import Node
 
 
 class NodeSerializer(serializers.ModelSerializer):
+
+    level_node = serializers.IntegerField(source='get_level_node', read_only=True)
     class Meta:
         model = Node
-        fields = ('id', 'path', 'project_id', 'item_type', 'item', 'inner_order', 'attributes')
+        fields = ('id', 'path', 'project_id', 'item_type', 'item', 'inner_order', 'attributes', 'level_node')
 
 
 class NewNodeSerializer(serializers.ModelSerializer):
     path = serializers.CharField(read_only=True)
     inner_order = serializers.IntegerField(read_only=True)
+    level_node = serializers.IntegerField(source='get_level_node', read_only=True)
 
     class Meta:
         model = Node
-        fields = ('id', 'path', 'project_id', 'item_type', 'item', 'inner_order', 'attributes')
+        fields = ('id', 'path', 'project_id', 'item_type', 'item', 'inner_order', 'attributes', 'level_node')
